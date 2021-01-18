2422dfb840d93f77d69b9ce83fa68587ecbdb23e
mollymoog <mmkover@gmail.com>
2021-01-18 14:55:01 -0500

    modify get requests

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,7 +6,7 @@ class ImagesController < ApplicationController
 
   # GET /images
   def index
-    @images = Image.all
+    @images = fetchImages()
 
     render json: @images
   end
@@ -51,4 +51,25 @@ class ImagesController < ApplicationController
     def image_params
       params.require(:image).permit(:url)
     end
+end
+
+def fetchImages() 
+  page_num = rand(1..40)
+  num_pics = 25
+  images_url = "https://picsum.photos/v2/list?page=#{page_num}&limit=#{num_pics}"
+
+  data = JSON.parse(RestClient.get("#{images_url}"))
+
+  data.each.with_index(1) do |image, index|
+      newer_image = Image.new(url: image["download_url"])
+      if index.even?
+          newer_image.team = "team1"
+      elsif index == 17
+          newer_image.team = "dead"
+      else
+         newer_image.team = "team2"
+      end
+      newer_image.save
+      binding.pry
+  end
 end
